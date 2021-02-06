87dcdef7d7d5d1dcda01c40149aba3e8f1285abf
crazyZhang <917724035@qq.com>
2021-02-06 16:57:08 +0800

    GetGrids_Memory, GetGrids_Screen

--- shell.py
+++ shell.py
@@ -1,7 +1,7 @@
 from board import FixSizeBoard, Grid
 from solver import Steps
 from memory_scan import GetGridsFromMemory
-from screen_scan import GameRegionRect
+from screen_scan import GameRegionRect, GetGridsFromScreen
 from windows import PositionToScreen, SetDpiAwareness, FindWindow, GetWindowThreadProcessId, WindowRect, Click
 
 
@@ -10,25 +10,39 @@ def ErrorExit(log):
     exit(-1)
 
 
-if __name__ == "__main__":
-    # fix screen-capture in different dpi
-    SetDpiAwareness()
-
+def GetGameWindow():
     windowTitle = "QQ游戏 - 连连看角色版"
     window = FindWindow(windowTitle)
     if window is None:
         ErrorExit(f"can not find window: {windowTitle}")
+    return window
 
-    thread, process = GetWindowThreadProcessId(window)
-    grids = GetGridsFromMemory(process)
-    grids = [grid if grid != 0 else Grid.Empty for grid in grids]
-    board = FixSizeBoard(19, 11)(list(grids))
 
+def GetGrids_Screen():
+    SetDpiAwareness()
+    window = GetGameWindow()
+    return GetGridsFromScreen(window)
+
+
+def GetGrids_Memory():
+    window = GetGameWindow()
+    _, process = GetWindowThreadProcessId(window)
+    return GetGridsFromMemory(process)
+
+
+def Solve(board):
     for step, board in Steps(board):
         startPosition, endPosition = step
         print(f"from {startPosition} to {endPosition}")
         print(board)
 
-        gameRect = GameRegionRect(WindowRect(window))
+        gameRect = GameRegionRect(WindowRect(GetGameWindow()))
         Click(PositionToScreen(startPosition, gameRect, 11, 19))
         Click(PositionToScreen(endPosition, gameRect, 11, 19))
+
+
+if __name__ == "__main__":
+    grids = GetGrids_Memory()
+    grids = [grid if grid != 0 else Grid.Empty for grid in grids]
+    board = FixSizeBoard(19, 11)(list(grids))
+    Solve(board)
